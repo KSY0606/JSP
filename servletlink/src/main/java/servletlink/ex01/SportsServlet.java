a9d5d80c65b4f2910f522e8218d5fd1f14a0cab1
KSY0606 <tkddbs9506@naver.com>
2022-09-20 23:55:49 +0900

    hidden, url rewriting

---
+++ servletlink/src/main/java/servletlink/ex01/SportsServlet.java
@@ -0,0 +1,33 @@
+package servletlink.ex01;
+
+import jakarta.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/sports") 							   // URL Rewriting 예제           LoginServlet2, login.html 같이 볼 것
+public class SportsServlet extends HttpServlet {
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		String user_id = request.getParameter("user_id");
+		out.print("<html><body>");
+		if(user_id != null && user_id.length() != 0) {
+			out.print(user_id + "님이 로그인한 상태<br>");
+			out.print("스포츠 중계 댓글을 작성 할 수 있습니다.");
+		}else {
+			out.print("로그인하지 않았습니다.<br>");
+			out.print("로그인해주세요<br>");
+			out.print("<a href='/servletlink/login.html'>로그인창으로 이동하기</a>");
+		}
+		out.print("</body></html>");
+	}
+
+}
