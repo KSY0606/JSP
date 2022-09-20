1aba5f36792e59622c8fdbedc94fc7a15d9f777b
KSY0606 <tkddbs9506@naver.com>
2022-09-20 23:52:50 +0900

    forward

---
+++ servletfw/src/main/java/servletfw/ex05/ContextFileServlet.java
@@ -0,0 +1,43 @@
+package servletfw.ex05;
+
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/cFile")
+public class ContextFileServlet extends HttpServlet {
+
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=utf-8");
+		PrintWriter out = response.getWriter();
+		ServletContext context = getServletContext();
+		InputStream inputs = context.getResourceAsStream("/WEB-INF/bin/init.txt");
+		BufferedReader bReader = new BufferedReader(new InputStreamReader(inputs));
+		String menu = null, menu_member = null, menu_order = null, menu_goods = null;
+		while((menu = bReader.readLine()) != null) {
+			StringTokenizer tokenizer = new StringTokenizer(menu, ",");
+			menu_member = tokenizer.nextToken();
+			menu_order = tokenizer.nextToken();
+			menu_goods = tokenizer.nextToken();
+		}
+		out.print("<html><body>");
+		out.print("<p>" + menu_member + "</p>");
+		out.print("<p>" + menu_order + "</p>");
+		out.print("<p>" + menu_goods + "</p>");
+		out.print("</body></html>");
+	}
+
+}
